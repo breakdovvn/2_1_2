31131655fb1079dfcea9552cca0968bd29be6ee4
Kamil <kamil.arslanoffx@gmail.com>
2025-03-22 01:34:12 +0500

    Коммит

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,27 @@
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld helloWorldBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorldBean1.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBean2.getMessage());
+
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getName());
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getName());
+
+        System.out.println("Сравнение бинов HelloWorld: " +
+                (helloWorldBean1 == helloWorldBean2));
+        System.out.println("Сравнение бинов Cat: " +
+                (catBean1 == catBean2));
     }
 }
